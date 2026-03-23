e09264431844e28be3e7c51db909032b753cfb7f
victoryy12 <victor3perez86@gmail.com>
2026-03-23 13:37:14 -0400

    fixed player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,19 @@
 extends RigidBody3D
 
-@export var player_speed: float = 20.0
-@export var jump_force: float = 8.0
-@export var max_speed: float = 10.0
-# Called when the node enters the scene tree for the first time.
+@export var rolling_force = 40
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	var dir = Vector3.ZERO
+	$CameraRig.top_level = true
+	
+func _physics_process(delta: float) -> void:
+	$CameraRig.global_transform.origin = global_transform.origin 
 	
 	if Input.is_action_pressed("up"):
-		dir += basis.z
-	if Input.is_action_pressed("down"):
-		dir -= basis.z
+		angular_velocity.x -= rolling_force * delta
+	elif Input.is_action_pressed("down"):
+		angular_velocity.x += rolling_force * delta
 	if Input.is_action_pressed("left"):
-		dir += basis.x
-	if Input.is_action_pressed("right"):
-		dir -= basis.x
-	
-	if dir != Vector3.ZERO:
-		apply_impulse(dir.normalized() * player_speed * delta)
-	
-	if Input.is_action_just_pressed("jump"):
-		apply_impulse(Vector3.UP * player_speed)
+		angular_velocity.z += rolling_force * delta
+	elif Input.is_action_pressed("right"):
+		angular_velocity.z -= rolling_force * delta
+		
