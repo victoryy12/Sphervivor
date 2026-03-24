293a4d8f047fc17d90d7521cb6ebcf61b3bc380c
victoryy12 <victor3perez86@gmail.com>
2026-03-23 21:46:03 -0400

    added jump and slim

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends RigidBody3D
 
 @export var rolling_force = 40
+@export var jump_force = 100
 
 func _ready() -> void:
 	$CameraRig.top_level = true
@@ -17,3 +18,8 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_pressed("right"):
 		angular_velocity.z -= rolling_force * delta
 		
+	if Input.is_action_pressed("jump"):
+		apply_central_impulse(Vector3.UP * jump_force)
+	if Input.is_action_pressed("slam"):
+		apply_central_force(Vector3.DOWN * 10000)
+	
