a2d41d669209e43d3a9f017601aef929dfb5a1a1
victoryy12 <victor3perez86@gmail.com>
2026-03-24 11:00:30 -0400

    main menu and movement

---
+++ scripts/main_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_setting_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed() -> void:
+	pass # Replace with function body.
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,36 @@
 extends RigidBody3D
 
 @export var rolling_force = 40
-@export var jump_force = 100
+@export var jump_force = 150
 
 func _ready() -> void:
 	$CameraRig.top_level = true
+	$touchingFloor.top_level = true
 	
 func _physics_process(delta: float) -> void:
-	$CameraRig.global_transform.origin = global_transform.origin 
-	
-	if Input.is_action_pressed("up"):
+	$CameraRig.global_transform.origin = global_transform.origin
+	$touchingFloor.global_transform.origin = global_transform.origin
+
+	#death plane (add death screen and death function at some point)
+	var deathBarrierDepth = -25
+	if global_position.y < deathBarrierDepth:
+		get_tree().reload_current_scene()
+
+	#movement 
+	var onFloor =  $touchingFloor.is_colliding()
+	#get some bugs when rolling while in the air
+	if Input.is_action_pressed("up") and onFloor:
 		angular_velocity.x -= rolling_force * delta
-	elif Input.is_action_pressed("down"):
+	elif Input.is_action_pressed("down") and onFloor:
 		angular_velocity.x += rolling_force * delta
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("left") and onFloor:
 		angular_velocity.z += rolling_force * delta
-	elif Input.is_action_pressed("right"):
+	elif Input.is_action_pressed("right") and onFloor:
 		angular_velocity.z -= rolling_force * delta
 		
-	if Input.is_action_pressed("jump"):
+	if Input.is_action_pressed("jump") and onFloor:
 		apply_central_impulse(Vector3.UP * jump_force)
-	if Input.is_action_pressed("slam"):
+	if Input.is_action_pressed("slam") and !onFloor:
+		angular_velocity.y -= rolling_force * delta
 		apply_central_force(Vector3.DOWN * 10000)
 	
